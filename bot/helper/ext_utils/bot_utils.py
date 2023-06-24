4a74b0c874187e498530edb566b775dbd6d45b24
yesiamshojib@gmail.com <yesiamshojib@gmail.com>
2023-06-24 22:14:22 +0600

    ok

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -138,7 +138,7 @@ def handleIndex(index, dic):
         elif index > 0: index = index - len(dic)
     return index
 
-def progress_bar(pct):
+def get_progress_bar_string(pct):
     pct = float(pct.strip('%'))
     p = min(max(pct, 0), 100)
     cFull = int(p / 10)
@@ -180,7 +180,7 @@ def get_readable_message():
         msg += f"by {download.extra_details['source']}\n\n"
         msg += f"<b>┌ {download.status()}...</b>"
         if download.status() not in [MirrorStatus.STATUS_SPLITTING, MirrorStatus.STATUS_SEEDING]:
-            msg += f"\n<b>├ {progress_bar(download.progress())}</b> {download.progress()}"
+            msg += f"\n<b>├ {get_progress_bar_string(download.progress())}</b> {download.progress()}"
             msg += f"\n<b>├ </b>{download.processed_bytes()} of {download.size()}"
             msg += f"\n<b>├ Speed</b>: {download.speed()}"
             msg += f'\n<b>├ Estimated</b>: {download.eta()}'
